23d58458fedc33b74edf37c7c9f97784742ee70a
Derek Anton <danton5276@gmail.com>
2014-05-25 11:41:57 -0700

    jframe gui basics.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class Main 
 {
+	
 	public static void main(String[] args)
 	{
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Ebay Snooper");
+		JLabel label = new JLabel();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
