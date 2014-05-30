a2f4f4c36a7304202d8a30564e0f13b9e9508b15
Danton <danton5276@gmail.com>
2014-05-30 13:07:50 -0400

    Large commit - Added JSoup, make sure to see readme to understand basic JAR linking in eclipse, Added what we need to fetch the DOM from the correct eBay webpage, added 3 basic CSS Selectors to fetch the following elements; item descriptions, item date times and item prices

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Website listing eBay URL syntax: http://www.helios825.org/url-parameters.php
 May want to look into jsoup.org. Looks like a good java library for easily parsing HTML.
 www.jsoup.org
 
+Took the liberty to add JSoup as explained. For your IDE you must add the newly included JAR file by going to Project>Properties>Java Build Path>Libraries and then adding the included jar file which lives at the top level directory of the project folder. 
--- src/eBayHandler.java
+++ src/eBayHandler.java
@@ -8,12 +8,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class eBayHandler {
 
 	private String queryString = "";
 	private String content = "";
-
+	private String builtUrl = "";
 	public eBayHandler(String s)
 	{
 		queryString = s;
@@ -25,16 +28,40 @@ public class eBayHandler {
 		URLConnection connection = null;
 		try 
 		{
-			connection =  new URL(buildURL()).openConnection();
-			Scanner scanner = new Scanner(connection.getInputStream());
+			builtUrl = buildURL(); // storing the built url for reference later on in the program //
+			
+			/*connection =  new URL(builtUrl).openConnection();
+			Scanner scanner = new Scanner(connection.getInputStream()); PULLED THIS FOR JSOUP IMPLEMENTATION
 			scanner.useDelimiter("\\Z");
 			content = scanner.next();
+			*/
+			Document dom = Jsoup.connect(builtUrl).get(); // A Document Object Module (DOM) object provided by JSoup //
+			
+			/* 
+			 *  CURRENTLY TESTING
+			 * 	using CSS selectors to properly select exactly what we need is something i need to do more research on
+			 *  currently just stupidly fetching ALL item descriptions, date times, and prices of the requested item
+			 *  
+			 *  Once i have a better handle at crafting CSS selector statements i can more accurately select what we need from the provided eBay page and then turn them into strings and present them to the user
+			 *  in the form of a notification or an email. 
+			 * 
+			 * */
+			
+			Elements itemDescriptions = dom.select("#ResultSetItems .vip"); // elements calls the select function on the Document object which takes a CSS selector string as a parameter //
+			Elements itemDateTimes = dom.select("#ResultSetItems .tme");
+			Elements itemPrices = dom.select("#ResultSetItems .prc");
+			System.out.print(""); // Debugging purpose only //
+			
+			
+			
+			
+			
 		} catch ( Exception ex )
 		{
 			ex.printStackTrace();
 		}
 		// To test received HTML
-		System.out.println(content);
+		// System.out.println(content);
 	}
 	
 	// Method that builds a URL to request eBay HTML
